c0eaf5f0cc4467d60bfb09e14af4963b13e83ed9
Mohammed Karimi <mohammed.karimi01@hotmail.com>
2022-10-27 14:27:38 +0200

    Pillat på Person klassen
    Pillat på Person klassen

--- Resturangen-Kod/Resturangen-Kod/Persons.cs
+++ Resturangen-Kod/Resturangen-Kod/Persons.cs
@@ -10,13 +10,6 @@ namespace Resturangen_Kod
     {
         public string Names { get; set; }
         //List<String> NameList = new List<String>();
-
-       
-        
-        
-        
-        
-        
         public Persons (string name)
         {
             Names = name;
@@ -25,15 +18,19 @@ namespace Resturangen_Kod
         {
             List<Persons> Guests = new List<Persons>();
             string[] ArrayNameList = {"Andersson", "Johansson", "Karlssson","Nillsson", "Eriksson"
-            , "Larsson","Olsson","Persson","Svensson","Gustavsson","Petterson","Johnsson","Jansson","Hansson","Bengtsson","Jönsson","Lindberg","Jakobsson","Magnusson","´Lindström"
+            ,"Larsson","Olsson","Persson","Svensson","Gustavsson","Petterson","Johnsson","Jansson","Hansson","Bengtsson","Jönsson","Lindberg","Jakobsson","Magnusson","´Lindström"
             ,"Olofsson","Lindkvist","Lindgren","Berg","Axelsson","Bergström","Lundberg","Lind","Lundgren","Lundqvist","Mattsson","Berglund","Fredriksson","Samberg","Henriksson"
             ,"Ali","Forsberg","Sjöberg","Walin","Engström","Eklund","Danielsson","Lundin","Håkansson","Björk","Bergman","Gunnarsson","Wikström","Holm","Samuelsson","Isaksson"
             ,"Fransson","Bergkvist","Nyström","Holmberg","Arvidsson","Lövgren","Söderberg","Nyberg","Ahmed","Blomqvist","Classon","Nordström","Hassan","Mårtensson","Lundström"
             ,"Viklund","Björklund","Eliasson","Berggren","Pålsson","Sandström","Nordin","Lund","Falk","Ström","Åberg","Ekström","Hermansson"};
+            int balance = 0;
+            Random rnd = new Random();
+            
             foreach (string name in ArrayNameList)
             {
+                balance = rnd.Next(50, 350);
                 Guests.Add(new Persons(name));
-                Console.WriteLine(name);
+                Console.WriteLine(name +" har "+ balance + "kr");
             }
          
         }
@@ -41,40 +38,53 @@ namespace Resturangen_Kod
     
     internal class Chefs : Persons
     {
-        public Chefs(string name) : base(name)
+        // Kockens kompetens är en rating från 1-10
+        public int Competence { get; set; }
+        public Chefs(string name, int competence) : base(name)
         {
-
+            Competence = competence;
         }
         public static void CreatingChefs()
         {
             List<Persons> Chefs = new List<Persons>();
             string[] ChefNames = { "Mohammed", "Thom", "Bilal", "Daniel", "Erik", };
+            Random rnd = new Random();
+
             foreach (string name in ChefNames)
             {
+                int _Competence = 1;
+                for (int i = 0; i < 1; i++)
+                {
+                    _Competence = rnd.Next(5, 11);
+                }
                 Chefs.Add(new Persons(name));
-                Console.WriteLine(name);
-                
             }
         }
-        // Kokens kompetens är en rating från 1-10
-        public int Competence { get; set; }
-        
     }
     internal class Waiters : Persons
     {
-        public Waiters(string name) : base(name)
+        public Waiters(string name, int servicelevel) : base(name)
         {
-
+            ServiceLevel = servicelevel;
         }
+        //public static int WaiterServiceLvL()
+        //{
+
+        //    return _ServiceLvL;
+        //}
         public static void CreatingWaiter()
         {
             List<Persons> Waiters = new List<Persons>();
             string[] WaiterNames = { "Elias", "Emma", "Kenneth", };
-            foreach(string name in WaiterNames)
+            Random rnd = new Random();
+            foreach (string name in WaiterNames)
             {
+                int _ServiceLvL = 1;
+                for (int i = 0; i < 1; i++)
+                {
+                    _ServiceLvL = rnd.Next(5,11);
+                }
                 Waiters.Add(new Persons(name));
-                Console.WriteLine(name);
-
             }
         }
 
@@ -84,13 +94,25 @@ namespace Resturangen_Kod
     }
     internal class Guests : Persons
     {
-        public Guests(string name) : base(name)
-        {
 
+        public Guests(string name, int groupsize, int amountofmoney, bool satisfaction) : base(name)
+        {
+            Names = name;
+            GroupSize = groupsize;
+            AmoutOfMoney = amountofmoney;
+            Satisfaction = satisfaction;
+
+        }
+        public static int GetMoney()
+        {
+            Random rnd = new Random();
+            int amountofmoney = 0;
+            amountofmoney = rnd.Next(100,1000);
+            return amountofmoney;
         }
 
         public int GroupSize { get; set; }
-        public int AmoutnOfMoney { get; set; }
+        public int AmoutOfMoney { get; set; }
         public bool Satisfaction { get; set; }
 
         //Ändrar på propen senare.
--- Resturangen-Kod/Resturangen-Kod/RunProgram.cs
+++ Resturangen-Kod/Resturangen-Kod/RunProgram.cs
@@ -11,6 +11,7 @@ namespace Resturangen_Kod
 
         public static void StartProgram()
         {
+            //Använder raderna under bara för att testa saker i Person klassen.
             //Persons.CreatingPersons();
             //Chefs.CreatingChefs();
             //Waiters.CreatingWaiter();
